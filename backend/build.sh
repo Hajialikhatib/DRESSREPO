c5a12d31200bcb9fcb5940d6493e04ae5eb40548
kijumbe005 <dogokjumb@gmail.com>
2026-03-03 02:26:54 -0800

    Use external DATABASE_URL and harden Render build for setuptools

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,7 @@ set -o nounset
 set -o pipefail
 
 python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools
 python -m pip install -r requirements.txt
 
 python manage.py migrate --noinput
